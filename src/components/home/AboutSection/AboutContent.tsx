d8c7f6cbc892082bdb4e033903fdaa6fffd14daa
Austin254 <austinmunesh@gmail.com>
2024-12-23 17:40:32 +0300

    changes

--- src/components/home/AboutSection/AboutContent.tsx
+++ src/components/home/AboutSection/AboutContent.tsx
@@ -13,8 +13,8 @@ export function AboutContent() {
     >
       <h2 className="text-5xl font-light mb-8">About Me</h2>
       <p className="text-neutral-600 mb-6 text-lg">
-        Everything has its beauty, but not everyone sees it (Andy Warhol).
-        As a certified makeup Artist I have had the privilege of seeing over 50 smiles over the past one year and as stated very well by Andy I take pride in enhancing and bringing out the beauty in different kinds of people that need it for different events. This includes Television shows, Bridal, birthdays, Corporate shots and special events and I now have an extensive portfolio comprising everything beauty.
+        Everything has its beauty, but not everyone sees it  (Andy Warhol).
+        As a certified makeup Artist I have had the privilege of seeing over 50 smiles over the past one year & as stated very well by Andy I take pride in enhancing and bringing out the beauty in different kinds of people that need it for different events. This includes Television shows, Bridal, birthdays, Corporate shots and special events and I now have an extensive portfolio comprising everything beauty.
       </p>
       <p className="text-neutral-600 mb-8 text-lg">
         My approach combines technical expertise with an artistic eye, ensuring each client 
